201365a8e4cc0310b1efb3635b05961044982b55
yukionishi1129 <yuki.onishi1129@gmail.com>
2024-03-22 22:38:43 +0900

    set firestore client

--- batch-service/server/main.go
+++ batch-service/server/main.go
@@ -1,9 +1,50 @@
 package main
 
-import "github.com/YukiOnishi1129/techpicks/batch-service/cmd/usecase"
+import (
+	"cloud.google.com/go/firestore"
+	"context"
+	firebase "firebase.google.com/go"
+	"github.com/joho/godotenv"
+	"google.golang.org/api/option"
+	"log"
+	"os"
+)
 
 func main() {
+	ctx := context.Background()
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+		return
+	}
+	client, err := createClient(ctx)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+		return
+	}
+	defer func(client *firestore.Client) {
+		err := client.Close()
+		if err != nil {
+
+		}
+	}(client)
+
 	//initializer.Init()
-	rss := usecase.GetRSS()
-	println("【rss count】: ", len(rss))
+	//rss := usecase.GetRSS()
+	//println("【rss count】: ", len(rss))
+}
+
+func createClient(ctx context.Context) (*firestore.Client, error) {
+	opt := option.WithCredentialsJSON([]byte(os.Getenv("GOOGLE_CREDENTIALS_JSON")))
+	app, err := firebase.NewApp(ctx, nil, opt)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+		return nil, err
+	}
+	client, err := app.Firestore(ctx)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+		return nil, err
+	}
+	return client, nil
 }
