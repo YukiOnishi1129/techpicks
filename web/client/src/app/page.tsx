a84b69004d2c4a0baff6711df787bb8e81b64405
yukionishi1129 <yuki.onishi1129@gmail.com>
2024-03-24 01:58:34 +0900

    ArticleCard component

--- web/client/src/app/page.tsx
+++ web/client/src/app/page.tsx
@@ -1,5 +1,6 @@
 import { getArticles } from "@/features/articles/repository/article";
 import Link from "next/link";
+import { ArticleCard } from "@/features/articles/components/ArticleCard";
 
 export default async function Home() {
   const articles = await getArticles({
@@ -8,20 +9,7 @@ export default async function Home() {
   return (
     <div className="w-[90%] h-auto mt-4 mx-auto border-2 grid grid-cols-3 gap-4">
       {articles.map((article) => (
-        <div key={article.id} className="cursor-pointer border-2">
-          <Link href={article.articleUrl} target="_blank">
-            <div className="w-full h-48 border-2 ">
-              <img
-                className="w-full h-full object-cover object-center"
-                src={article.thumbnailURL}
-                alt=""
-              />
-            </div>
-            <div className="w-full h-1/4 border-2 ">
-              <h3>{article.title}</h3>
-            </div>
-          </Link>
-        </div>
+        <ArticleCard key={article.id} article={article} />
       ))}
     </div>
   );
---
+++ web/client/src/features/articles/components/ArticleCard.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import { FC } from "react";
+import { Article } from "@/types/article";
+
+type ArticleCardProps = {
+  article: Article;
+};
+
+export const ArticleCard: FC<ArticleCardProps> = ({
+  article,
+}: ArticleCardProps) => {
+  return (
+    <div className="cursor-pointer border-2">
+      <Link href={article.articleUrl} target="_blank">
+        <div className="w-full h-48 border-2 ">
+          <img
+            className="w-full h-full object-cover object-center"
+            src={article.thumbnailURL}
+            alt=""
+          />
+        </div>
+        <div className="w-full h-1/4 border-2 ">
+          <h3>{article.title}</h3>
+        </div>
+      </Link>
+    </div>
+  );
+};
