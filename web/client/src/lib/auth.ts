6cddc2d27965dee8066eb8be38a1b2c7961d546d
yukionishi1129 <yuki.onishi1129@gmail.com>
2024-03-26 23:46:19 +0900

    firestore adaptor

--- web/client/src/lib/auth.ts
+++ web/client/src/lib/auth.ts
@@ -1,8 +1,13 @@
+import { FirestoreAdapter } from "@next-auth/firebase-adapter";
+import { cert } from "firebase-admin/app";
 import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions: NextAuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
+  adapter: FirestoreAdapter({
+    credential: cert(JSON.parse(process.env.GOOGLE_CREDENTIALS_JSON as string)),
+  }),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
