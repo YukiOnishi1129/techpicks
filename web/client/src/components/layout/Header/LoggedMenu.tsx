f4297e72b4777c2ae8d312e3166577ae5a679e87
yukionishi1129 <yuki.onishi1129@gmail.com>
2024-03-30 00:02:02 +0900

    avater

--- web/client/src/components/layout/Header/LoggedMenu.tsx
+++ web/client/src/components/layout/Header/LoggedMenu.tsx
@@ -2,10 +2,24 @@
 import { signOut } from "next-auth/react";
 
 import { Button } from "@/components/ui/button";
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import { Session } from "next-auth";
+import { FC } from "react";
 
-export const LoggedMenu = () => {
+type LoggedMenuProps = {
+  session: Session | null;
+};
+
+export const LoggedMenu: FC<LoggedMenuProps> = ({
+  session,
+}: LoggedMenuProps) => {
+  const image = session?.user?.image || "no_image.png";
   return (
-    <div>
+    <div className="grid grid-cols-2">
+      <Avatar>
+        <AvatarImage src={image} alt="avatar" />
+        <AvatarFallback>{session?.user?.name}</AvatarFallback>
+      </Avatar>
       <Button onClick={() => signOut()}>Logout</Button>
     </div>
   );
--- web/client/src/components/layout/Header/index.tsx
+++ web/client/src/components/layout/Header/index.tsx
@@ -3,16 +3,19 @@ import { getServerSession } from "next-auth/next";
 
 import { LoggedMenu } from "./LoggedMenu";
 import { NotLoggedMenu } from "./NotLoggedMenu";
+import { authOptions } from "@/lib/auth";
 
 export async function Header() {
-  const session = await getServerSession();
+  const session = await getServerSession(authOptions);
   return (
     <div className="fixed flex justify-between px-8 w-screen h-16 items-center bg-white border-b border-gray-300 shadow-md">
       <Link href="/" className="cursor-pointer">
         <h1 className="font-bold text-2xl">Tech Picks</h1>
       </Link>
 
-      <div className="flex">{session ? <LoggedMenu /> : <NotLoggedMenu />}</div>
+      <div className="flex">
+        {session ? <LoggedMenu session={session} /> : <NotLoggedMenu />}
+      </div>
     </div>
   );
 }
