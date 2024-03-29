564b6fa03a5abd4f8fada0522d24068edb6ad8dd
yukionishi1129 <yuki.onishi1129@gmail.com>
2024-03-29 23:31:26 +0900

    sidebar fix

--- web/client/src/components/layout/BaseLayout/index.tsx
+++ web/client/src/components/layout/BaseLayout/index.tsx
@@ -1,13 +1,23 @@
 import { ReactNode } from "react";
 
 import { Header } from "../Header";
+import { Sidebar } from "../Sidebar";
 
 export const BaseLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div>
-      <Header />
-      <div className="h-16"></div>
-      <main className="">{children}</main>
+    <div className="overflow-hidden">
+      <header className="overflow-hidden">
+        <Header />
+      </header>
+
+      <div className="h-16" />
+      <main className="flex">
+        <div className="fixed w-[200px] h-lvh">
+          <Sidebar />
+        </div>
+        <div className="w-[200px] mr-[10px]" />
+        <div className="w-full mx-auto">{children}</div>
+      </main>
     </div>
   );
 };
--- web/client/src/components/layout/Sidebar/index.tsx
+++ web/client/src/components/layout/Sidebar/index.tsx
@@ -2,11 +2,22 @@ import Link from "next/link";
 
 export function Sidebar() {
   return (
-    <div>
-      <ul>
-        <Link href="/">Today</Link>
-        <Link href="/bookmark">Read Later</Link>
-      </ul>
+    <div className="mr-4 pb-12 w-full border-r-2">
+      <div className="space-y-4 py-4">
+        <div className="px-4 py-2">
+          <h2 className="mb-2 px-2 text-lg font-semibold tracking-tight">
+            Main
+          </h2>
+          <div className="space-y-1 pl-8">
+            <div>
+              <Link href="/">Today</Link>
+            </div>
+            <div>
+              <Link href="/bookmark">Read Later</Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
