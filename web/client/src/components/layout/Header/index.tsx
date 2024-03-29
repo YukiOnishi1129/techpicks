b78838981288b1a62c16b6480b0d5a4ced734bfa
yukionishi1129 <yuki.onishi1129@gmail.com>
2024-03-30 00:36:43 +0900

    opacity

--- web/client/src/components/layout/Header/index.tsx
+++ web/client/src/components/layout/Header/index.tsx
@@ -8,7 +8,7 @@ import { authOptions } from "@/lib/auth";
 export async function Header() {
   const session = await getServerSession(authOptions);
   return (
-    <div className="fixed flex justify-between px-8 w-screen h-16 items-center bg-white border-b border-gray-300 shadow-md">
+    <div className="z-50 fixed flex justify-between px-8 w-screen h-16 items-center bg-white border-b border-gray-300 shadow-md">
       <Link href="/" className="cursor-pointer">
         <h1 className="font-bold text-2xl">Tech Picks</h1>
       </Link>
--- web/client/src/features/articles/components/ArticleCard.tsx
+++ web/client/src/features/articles/components/ArticleCard.tsx
@@ -17,22 +17,24 @@ export const ArticleCard: FC<ArticleCardProps> = ({
   const imageUrl = useCheckImageExist(article.thumbnailURL);
 
   return (
-    <div className="cursor-pointer w-full rounded hover:opacity-30">
+    <div className="cursor-pointer relative w-full rounded hover:opacity-30">
       <Link href={article.articleUrl} target="_blank">
         <div className="grid grid-cols-2">
-          <div className="w-[90%] h-48">
+          <div className="w-[90%] h-48 flex justify-center">
             <img
-              className="h-full object-cover object-center rounded"
+              className="h-full object-cover object-center rounded border-2 shadow-md"
               src={imageUrl}
               alt=""
             />
           </div>
           <div>
-            <div className="w-full h-1/4">
-              <h3>{article.title}</h3>
+            <div className="w-full h-1/4 mb-4 line-clamp-3">
+              <h3 className="font-bold text-lg">{article.title}</h3>
             </div>
-            <div>
-              <p>{article.platform.name}</p>
+            <div className="py-2">
+              <span className="px-2 py-1 rounded-lg text-white bg-sky-500">
+                {article.platform.name}
+              </span>
             </div>
             <p>{formatDate(article.publishedAt)}</p>
           </div>
