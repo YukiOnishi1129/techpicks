374f800a8338d4cd4a309f6276ca339fe2067d9c
yukionishi1129 <yuki.onishi1129@gmail.com>
2024-04-07 01:06:12 +0900

    policy

--- supabase/migrations/20240404085103_create_auth_tables.sql
+++ supabase/migrations/20240404085103_create_auth_tables.sql
@@ -1,6 +1,3 @@
--- CREATE FUNCTION set_profiles_update_time()
---     RETURNS TRIGGER AS $$ BEGIN NEW.updated_at = NOW(); RETURN NEW; END; $$ LANGUAGE plpgsql;
-
 -- CreateTable
 CREATE TABLE "profiles" (
     "id" uuid REFERENCES auth.users NOT NULL,
@@ -12,7 +9,6 @@ CREATE TABLE "profiles" (
     "is_super_admin" BOOLEAN NULL,
     "created_at" TIMESTAMP(3) NULL,
     "updated_at" TIMESTAMP(3) NULL,
-    -- "deleted_at" TIMESTAMP(3),
 
     CONSTRAINT "profiles_pkey" PRIMARY KEY ("id")
 );
@@ -20,42 +16,22 @@ CREATE TABLE "profiles" (
 -- CreateIndex
 -- CREATE UNIQUE INDEX "profiles_email_key" ON "profiles"("email");
 
--- CREATE TRIGGER profiles_update_tri BEFORE UPDATE ON profiles FOR EACH ROW EXECUTE PROCEDURE set_profiles_update_time();
-
 alter table profiles enable row level security;
 
--- create policy "Public profiles are viewable by everyone."
---   on profiles for select
---   using ( true );
+create policy "Public profiles are viewable by everyone."
+  on profiles for select
+  using ( true );
 
--- create policy "Users can insert their own profile."
---   on profiles for insert
---   with check ( auth.uid() = id );
+create policy "Users can insert their own profile."
+  on profiles for insert
+  with check ( auth.uid() = id );
 
--- create policy "Users can update own profile."
---   on profiles for update
---   using ( auth.uid() = id );
+create policy "Users can update own profile."
+  on profiles for update
+  using ( auth.uid() = id );
 
 
 -- inserts a row into public.profiles
--- CREATE OR REPLACE FUNCTION public.create_profile_for_user()
---  RETURNS trigger
---  LANGUAGE plpgsql
---  SECURITY DEFINER
--- AS $$
--- BEGIN
---   INSERT INTO public.profiles (id, email, first_name, last_name, avatar_url)
---   SELECT
---     new.id,
---     new.email,
---     COALESCE(split_part(jsonb_extract_path_text(new.raw_user_meta_data, 'full_name'), ' ', 1), NULL),
---     COALESCE(split_part(jsonb_extract_path_text(new.raw_user_meta_data, 'full_name'), ' ', 2), NULL),
---     COALESCE(jsonb_extract_path_text(new.raw_user_meta_data, 'picture'), jsonb_extract_path_text(new.raw_user_meta_data, 'avatar_url'), NULL);
---   RETURN new;
--- END;
--- $$
-
-
 create function public.handle_new_user()
 returns trigger as $$
 begin
