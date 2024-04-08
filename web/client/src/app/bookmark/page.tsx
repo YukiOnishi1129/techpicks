3258ada2130bffd9a49718b3514e184f9bb5b24b
yukionishi1129 <yuki.onishi1129@gmail.com>
2024-04-08 23:17:38 +0900

    bookmark type

--- web/client/src/app/bookmark/page.tsx
+++ web/client/src/app/bookmark/page.tsx
@@ -1,9 +1,5 @@
+import { BookmarkList } from "@/features/bookmarks/components/BookmarkList";
+
 export default function Bookmark() {
-  return (
-    <div className="mx-auto mt-20 w-2/5 rounded-md border-2 border-gray-300 p-4 shadow-md">
-      <h2 className="text-center text-2xl font-bold text-gray-800">
-        Bookmark Page
-      </h2>
-    </div>
-  );
+  return <BookmarkList />;
 }
