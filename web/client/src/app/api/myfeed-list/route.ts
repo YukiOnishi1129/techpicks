14218847de43b190fa791ba22dabf4fc8221d968
yukionishi1129 <yuki.onishi1129@gmail.com>
2024-04-19 21:05:43 +0900

    getMyFeedList api

---
+++ web/client/src/app/api/myfeed-list/route.ts
@@ -0,0 +1,33 @@
+"use server";
+import { NextRequest, NextResponse } from "next/server";
+
+import { getMyFeedList } from "@/features/myFeedList/repository/myFeedList";
+import { getUser } from "@/features/users/actions/user";
+
+export async function GET(req: NextRequest) {
+  const user = await getUser();
+  if (!user) {
+    return NextResponse.json(
+      {
+        message: "unauthorized",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+
+  const myFeedLists = await getMyFeedList({
+    userId: user.id,
+  });
+
+  return NextResponse.json(
+    {
+      myFeedLists: myFeedLists,
+      message: "success",
+    },
+    {
+      status: 200,
+    }
+  );
+}
