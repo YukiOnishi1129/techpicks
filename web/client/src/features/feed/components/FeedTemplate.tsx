d85cc26a8bdf45aaff9a88a357c20ee7ec36efd5
yukionishi1129 <yuki.onishi1129@gmail.com>
2024-04-19 22:30:17 +0900

    create FeedTemplate

---
+++ web/client/src/features/feed/components/FeedTemplate.tsx
@@ -0,0 +1,8 @@
+export const FeedTemplate = () => {
+  return (
+    <div className="w-auto">
+      <h1 className="mb-4 mt-8 text-2xl font-bold text-gray-800">Feed List</h1>
+      <div className="w-full border-b-2 bg-white py-4"></div>
+    </div>
+  );
+};
