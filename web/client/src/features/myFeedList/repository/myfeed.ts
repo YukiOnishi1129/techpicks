8cfe432e1dddbf4de8db518c6e9191e58af2f0f4
yukionishi1129 <yuki.onishi1129@gmail.com>
2024-04-19 21:02:09 +0900

    getMyFeedListById

--- web/client/src/features/myFeedList/repository/myfeed.ts
+++ web/client/src/features/myFeedList/repository/myfeed.ts
@@ -116,7 +116,111 @@ export const getMyFeedList = async ({ userId }: GetMyFeedList) => {
   }
 };
 
-export const getMyFeedListById = async (id: string) => {};
+export const getMyFeedListById = async (id: string) => {
+  try {
+    const myFeedList = await prisma.myFeedList.findUnique({
+      where: {
+        id: id,
+      },
+      include: {
+        myFeeds: {
+          select: {
+            feed: {
+              select: {
+                id: true,
+                name: true,
+                siteUrl: true,
+                rssUrl: true,
+                isTrending: true,
+                createdAt: true,
+                updatedAt: true,
+                deletedAt: true,
+                category: {
+                  select: {
+                    id: true,
+                    name: true,
+                    type: true,
+                    createdAt: true,
+                    updatedAt: true,
+                    deletedAt: true,
+                  },
+                },
+                platform: {
+                  select: {
+                    id: true,
+                    name: true,
+                    siteUrl: true,
+                    faviconUrl: true,
+                    platformType: true,
+                    isEng: true,
+                    createdAt: true,
+                    updatedAt: true,
+                  },
+                },
+              },
+            },
+          },
+        },
+        profile: {
+          select: {
+            id: true,
+            name: true,
+            email: true,
+            image: true,
+            createdAt: true,
+            updatedAt: true,
+          },
+        },
+      },
+    });
+
+    if (!myFeedList) {
+      throw new Error(`My feed list not found`);
+    }
+
+    const resMyFeedList: MyFeedListType = {
+      id: myFeedList.id,
+      title: myFeedList.title,
+      description: myFeedList.description,
+      createdAt: myFeedList.createdAt,
+      updatedAt: myFeedList.updatedAt,
+      profile: {
+        id: myFeedList.profile.id,
+        name: myFeedList.profile.name,
+        email: myFeedList.profile.email,
+        image: myFeedList.profile.image,
+        createdAt: myFeedList.profile.createdAt,
+        updatedAt: myFeedList.profile.updatedAt,
+      },
+      feeds: myFeedList.myFeeds.map((myFeed) => {
+        return {
+          id: myFeed.feed.id,
+          name: myFeed.feed.name,
+          siteUrl: myFeed.feed.siteUrl,
+          rssUrl: myFeed.feed.rssUrl,
+          isTrending: myFeed.feed.isTrending,
+          createdAt: myFeed.feed.createdAt,
+          updatedAt: myFeed.feed.updatedAt,
+          category: myFeed.feed.category,
+          platform: {
+            id: myFeed.feed.platform.id,
+            name: myFeed.feed.platform.name,
+            siteUrl: myFeed.feed.platform.siteUrl,
+            faviconUrl: myFeed.feed.platform.faviconUrl,
+            platformType: myFeed.feed.platform.platformType,
+            isEng: myFeed.feed.platform.isEng,
+            createdAt: myFeed.feed.platform.createdAt,
+            updatedAt: myFeed.feed.platform.updatedAt,
+          },
+        };
+      }),
+    };
+
+    return resMyFeedList;
+  } catch (err) {
+    throw new Error(`Failed to get my feed list by id: ${err}`);
+  }
+};
 
 type createMyFeedListDTO = {
   title: string;
