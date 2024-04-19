9fee1cb66844c7e1c4b5958ea5a511ab7289d98f
yukionishi1129 <yuki.onishi1129@gmail.com>
2024-04-19 21:19:37 +0900

    fetchMyFeedListById

--- web/client/src/features/myFeedList/actions/myFeedList.ts
+++ web/client/src/features/myFeedList/actions/myFeedList.ts
@@ -41,3 +41,50 @@ export const fetchMyFeedList = async () => {
     };
   }
 };
+
+export type FetchMyFeedListByIdAPIResponse = {
+  data: {
+    myFeedList?: MyFeedListType;
+    message: string;
+  };
+  status: number;
+};
+
+export const fetchMyFeedListById = async (id: string) => {
+  const url = `http://localhost:80/api/myfeed-list/${id}`;
+  const response = await getFetch({
+    url,
+    tagName: "myfeed-list",
+    cacheType: "no-store",
+  });
+  const data = await response.json();
+  const status = response.status;
+
+  if (status === 401) {
+    return {
+      data: {
+        myFeedList: undefined,
+        message: data.message as string,
+      },
+      status: status,
+    };
+  }
+
+  if (status === 404) {
+    return {
+      data: {
+        myFeedList: undefined,
+        message: data.message as string,
+      },
+      status: status,
+    };
+  }
+
+  return {
+    data: {
+      myFeedList: data?.myFeedList as MyFeedListType,
+      message: data.message as string,
+    },
+    status: status,
+  };
+};
