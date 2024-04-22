09704d270d2b9465a31566b1e6b57f02ed87f5a6
yukionishi1129 <yuki.onishi1129@gmail.com>
2024-04-22 22:53:31 +0900

    hook form

--- web/client/src/features/feeds/components/DropdownMenu/FollowDropdownMenu.tsx
+++ web/client/src/features/feeds/components/DropdownMenu/FollowDropdownMenu.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import { zodResolver } from "@hookform/resolvers/zod";
 import { FC, useCallback, useMemo } from "react";
+import { useForm, Controller } from "react-hook-form";
+import { z } from "zod";
 
 import { CreateMyFeedListDialog } from "@/features/myFeedLists/components/Dialog";
 
@@ -12,6 +15,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { Input } from "@/components/ui/input";
 
 import { MyFeedListType } from "@/types/myFeedList";
 
@@ -64,31 +68,86 @@ export async function FollowDropdownMenu({
           </Button>
         )}
       </DropdownMenuTrigger>
-
-      <DropdownMenuContent align="end" className="w-[200px]">
-        <DropdownMenuSeparator />
-        <div className="max-h-[200px] overflow-y-auto">
-          {sortedMyFeedLists.length &&
-            sortedMyFeedLists.map((myFeedList) => (
-              <TargetFollowMyFeedList
-                key={`${feedId}-${myFeedList.id}`}
-                feedId={feedId}
-                myFeedList={myFeedList}
-                handleCreateMyFeed={handleCreateMyFeed}
-                handleRemoveMyFeed={handleRemoveMyFeed}
-              />
-            ))}
-        </div>
-
-        <DropdownMenuLabel>
-          <CreateMyFeedListDialog
-            handleCreatedMyFeedLists={handleCreatedMyFeedLists}
-          />
-        </DropdownMenuLabel>
-      </DropdownMenuContent>
+      <FollowDorpDownMenuContent
+        feedId={feedId}
+        myFeedLists={sortedMyFeedLists}
+        handleCreateMyFeed={handleCreateMyFeed}
+        handleRemoveMyFeed={handleRemoveMyFeed}
+        handleCreatedMyFeedLists={handleCreatedMyFeedLists}
+      />
     </DropdownMenu>
   );
 }
+
+type FollowDorpDownMenuContentProps = {
+  feedId: string;
+  myFeedLists: Array<MyFeedListType>;
+  handleCreateMyFeed: (myFeedListId: string) => Promise<string | undefined>;
+  handleRemoveMyFeed: (
+    myFeedId: string,
+    myFeedListId: string
+  ) => Promise<string | undefined>;
+  handleCreatedMyFeedLists: (myFeedId: string) => Promise<void>;
+};
+
+const formSchema = z.object({
+  keyword: z.string(),
+});
+
+const FollowDorpDownMenuContent: FC<FollowDorpDownMenuContentProps> = ({
+  feedId,
+  myFeedLists,
+  handleCreateMyFeed,
+  handleRemoveMyFeed,
+  handleCreatedMyFeedLists,
+}: FollowDorpDownMenuContentProps) => {
+  const { getValues, control, watch } = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      keyword: "",
+    },
+  });
+
+  const searchKeyword = watch("keyword");
+
+  const showMyFeedLists = useMemo(() => {
+    if (!searchKeyword) return myFeedLists;
+    return myFeedLists.filter((myFeedList) =>
+      myFeedList.title.includes(searchKeyword)
+    );
+  }, [myFeedLists, searchKeyword]);
+
+  return (
+    <DropdownMenuContent align="end" className="w-[200px]">
+      <Controller
+        name="keyword"
+        control={control}
+        rules={{ required: true }}
+        render={({ field }) => (
+          <Input placeholder="search keyword" {...field} />
+        )}
+      />
+
+      <DropdownMenuSeparator />
+      <div className="max-h-[200px] overflow-y-auto">
+        {showMyFeedLists.map((myFeedList) => (
+          <TargetFollowMyFeedList
+            key={`${feedId}-${myFeedList.id}`}
+            feedId={feedId}
+            myFeedList={myFeedList}
+            handleCreateMyFeed={handleCreateMyFeed}
+            handleRemoveMyFeed={handleRemoveMyFeed}
+          />
+        ))}
+      </div>
+      <DropdownMenuLabel>
+        <CreateMyFeedListDialog
+          handleCreatedMyFeedLists={handleCreatedMyFeedLists}
+        />
+      </DropdownMenuLabel>
+    </DropdownMenuContent>
+  );
+};
 
 type TargetFollowMyFeedListProps = {
   feedId: string;
