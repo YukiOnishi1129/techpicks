0db899074d1b6c4e08e9c7493ba30d928f9afce7
yukionishi1129 <yuki.onishi1129@gmail.com>
2024-04-22 23:04:37 +0900

    refactoring

---
+++ web/client/src/features/feeds/components/DropdownMenu/FollowDropdownMenu/FollowDorpDownMenuContent.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FC, useMemo } from "react";
+import { useForm, Controller } from "react-hook-form";
+import { z } from "zod";
+
+import { CreateMyFeedListDialog } from "@/features/myFeedLists/components/Dialog";
+
+import {
+  DropdownMenuContent,
+  DropdownMenuSeparator,
+  DropdownMenuLabel,
+} from "@/components/ui/dropdown-menu";
+import { Input } from "@/components/ui/input";
+
+import { MyFeedListType } from "@/types/myFeedList";
+
+import { TargetFollowMyFeedList } from "./FollowTargetMyFeedList";
+
+type FollowDorpDownMenuContentProps = {
+  feedId: string;
+  myFeedLists: Array<MyFeedListType>;
+  handleCreateMyFeed: (myFeedListId: string) => Promise<string | undefined>;
+  handleRemoveMyFeed: (
+    myFeedId: string,
+    myFeedListId: string
+  ) => Promise<string | undefined>;
+  handleCreatedMyFeedLists: (myFeedId: string) => Promise<void>;
+};
+
+const formSchema = z.object({
+  keyword: z.string(),
+});
+
+export const FollowDorpDownMenuContent: FC<FollowDorpDownMenuContentProps> = ({
+  feedId,
+  myFeedLists,
+  handleCreateMyFeed,
+  handleRemoveMyFeed,
+  handleCreatedMyFeedLists,
+}: FollowDorpDownMenuContentProps) => {
+  const { getValues, control, watch } = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      keyword: "",
+    },
+  });
+
+  const searchKeyword = watch("keyword");
+
+  const showMyFeedLists = useMemo(() => {
+    if (!searchKeyword) return myFeedLists;
+    return myFeedLists.filter((myFeedList) =>
+      myFeedList.title.includes(searchKeyword)
+    );
+  }, [myFeedLists, searchKeyword]);
+
+  return (
+    <DropdownMenuContent align="end" className="w-[200px]">
+      <Controller
+        name="keyword"
+        control={control}
+        rules={{ required: true }}
+        render={({ field }) => (
+          <Input placeholder="search keyword" {...field} />
+        )}
+      />
+
+      <DropdownMenuSeparator />
+      <div className="max-h-[200px] overflow-y-auto">
+        {showMyFeedLists.map((myFeedList) => (
+          <TargetFollowMyFeedList
+            key={`${feedId}-${myFeedList.id}`}
+            feedId={feedId}
+            myFeedList={myFeedList}
+            handleCreateMyFeed={handleCreateMyFeed}
+            handleRemoveMyFeed={handleRemoveMyFeed}
+          />
+        ))}
+      </div>
+      <DropdownMenuLabel>
+        <CreateMyFeedListDialog
+          handleCreatedMyFeedLists={handleCreatedMyFeedLists}
+        />
+      </DropdownMenuLabel>
+    </DropdownMenuContent>
+  );
+};
---
+++ web/client/src/features/feeds/components/DropdownMenu/FollowDropdownMenu/FollowDropdownMenu.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import { MyFeedListType } from "@/types/myFeedList";
+
+import { FollowDorpDownMenuContent } from "./FollowDorpDownMenuContent";
+
+type FollowDropdownMenuProps = {
+  feedId: string;
+  isFollowing: boolean | undefined;
+  myFeedLists: Array<MyFeedListType>;
+  handleCreateMyFeed: (myFeedListId: string) => Promise<string | undefined>;
+  handleRemoveMyFeed: (
+    myFeedId: string,
+    myFeedListId: string
+  ) => Promise<string | undefined>;
+  handleCreatedMyFeedLists: (myFeedId: string) => Promise<void>;
+};
+
+export async function FollowDropdownMenu({
+  feedId,
+  isFollowing,
+  myFeedLists,
+  handleCreateMyFeed,
+  handleRemoveMyFeed,
+  handleCreatedMyFeedLists,
+}: FollowDropdownMenuProps) {
+  const sortedMyFeedLists = myFeedLists.sort((prev, next) => {
+    if (prev.createdAt < next.createdAt) return -1;
+    if (prev.createdAt > next.createdAt) return 1;
+    return 0;
+  });
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        {isFollowing ? (
+          <Button
+            variant="outline"
+            size="sm"
+            className="group relative font-bold "
+          >
+            <span className="w-full group-hover:invisible">{"FOLLOWING"}</span>
+            <span className="invisible absolute w-full group-hover:visible">
+              {"EDIT"}
+            </span>
+          </Button>
+        ) : (
+          <Button
+            variant="outline"
+            size="sm"
+            className="border-emerald-500 font-bold text-emerald-500 hover:text-emerald-600"
+          >
+            {"FOLLOW"}
+          </Button>
+        )}
+      </DropdownMenuTrigger>
+      <FollowDorpDownMenuContent
+        feedId={feedId}
+        myFeedLists={sortedMyFeedLists}
+        handleCreateMyFeed={handleCreateMyFeed}
+        handleRemoveMyFeed={handleRemoveMyFeed}
+        handleCreatedMyFeedLists={handleCreatedMyFeedLists}
+      />
+    </DropdownMenu>
+  );
+}
