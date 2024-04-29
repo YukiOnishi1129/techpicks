3440f38256b45f064673fd8af078993899d6ad91
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2024-04-29 15:42:46 +0900

    navigation

--- web/client/src/components/layout/Header/MobileHeader/index.tsx
+++ web/client/src/components/layout/Header/MobileHeader/index.tsx
@@ -1,6 +1,9 @@
 "use client";
 import { User } from "@supabase/supabase-js";
 import { usePathname, useSearchParams } from "next/navigation";
+import { useMemo } from "react";
+
+import { NAVIGATION_LISTS } from "@/constant/navigation";
 
 import { ModeToggle } from "../DropdownMenu";
 
@@ -12,10 +15,25 @@ export async function MobileHeader({ user }: MobileHeaderProps) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
 
+  const pageName = useMemo(() => {
+    switch (pathname) {
+      case NAVIGATION_LISTS.BOOKMARK:
+        return "Bookmark";
+      case NAVIGATION_LISTS.HOME:
+        return "Today";
+      case NAVIGATION_LISTS.ARTICLE_SEARCH:
+        return "Search";
+      case NAVIGATION_LISTS.ARTICLE_SEARCH_RESULT:
+        return "Search Result";
+      default:
+        return "Tech Picks";
+    }
+  }, [pathname]);
+
   return (
     <div className="fixed z-50 flex h-16 w-screen items-center justify-between border-b border-gray-300  px-8 shadow-md">
       <div></div>
-      <h1 className="text-2xl font-bold ">Today</h1>
+      <h1 className="text-2xl font-bold ">{pageName}</h1>
       <div className="flex items-center justify-end">
         <div>
           <ModeToggle />
