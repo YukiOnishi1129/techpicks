44a054b12e729989e10d8eb57a622ae2b179d03e
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2024-05-01 16:58:02 +0900

    layout fix

--- web/client/src/features/articles/components/ArticleDetailSheet.tsx
+++ web/client/src/features/articles/components/ArticleDetailSheet.tsx
@@ -5,10 +5,7 @@ import { FC, useState } from "react";
 import { FcBookmark } from "react-icons/fc";
 import { MdOutlineBookmarkAdd } from "react-icons/md";
 
-import {
-  FeedCategoryNameBadge,
-  PlatformNameBadge,
-} from "@/components/ui/badge";
+import { FeedNameBadge } from "@/components/ui/badge/FeedNameBadge";
 import { Button } from "@/components/ui/button";
 import {
   Sheet,
@@ -16,14 +13,13 @@ import {
   SheetHeader,
   SheetTitle,
   SheetTrigger,
-  SheetFooter,
   SheetClose,
 } from "@/components/ui/sheet";
 
 import { useCheckImageExist } from "@/hooks/useImage";
 import { useParseHtml } from "@/hooks/useParseHtml";
 
-import { formatShowDateTime } from "@/lib/date";
+import { showDiffDateToCurrentDate } from "@/lib/date";
 
 import { ArticleType } from "@/types/article";
 
@@ -58,7 +54,7 @@ export const ArticleDetailSheet: FC<ArticleDetailSheetProps> = ({
           {children}
         </div>
       </SheetTrigger>
-      <SheetContent>
+      <SheetContent className="w-[360px] sm:w-[700px] sm:max-w-[700px]">
         {open && <ArticleContent article={article} user={user} />}
       </SheetContent>
     </Sheet>
@@ -79,123 +75,126 @@ const ArticleContent = ({
 
   return (
     <>
-      <SheetHeader>
-        <SheetTitle>
+      <SheetHeader className="mb-4">
+        <SheetTitle className="flex">
           <Link
-            className="cursor-pointer text-lg font-semibold  tracking-wide hover:text-blue-500"
+            className="inline-block w-full cursor-pointer  text-lg font-semibold tracking-wide hover:text-blue-500"
             href={article.articleUrl}
             target="_blank"
           >
-            <h1 className="pb-4">{article.title}</h1>
+            <span className="pb-4">{article.title}</span>
           </Link>
         </SheetTitle>
-        <div className="pb-0">
+
+        <p className="pl-2 text-sm">
+          {showDiffDateToCurrentDate(article.publishedAt)}
+        </p>
+
+        <div className="flex w-full items-center justify-between pt-2">
           <Link
-            className="hover:opacity-80"
+            className="mb-2 hover:opacity-80"
             href={article.platform?.siteUrl || ""}
             target="_blank"
           >
             {/* eslint-disable-next-line @next/next/no-img-element */}
             <img
-              className="mr-2 inline-block size-[24px]"
+              className="mr-2 inline-block size-[36px]"
               src={article.platform?.faviconUrl || ""}
               alt=""
             />
           </Link>
-          <Link
-            className="hover:opacity-80"
-            href={article.platform?.siteUrl || ""}
-            target="_blank"
-          >
-            <PlatformNameBadge name={article.platform.name} />
-          </Link>
+          {user && (
+            <div className="flex justify-end">
+              {bookmarkId ? (
+                <Button
+                  variant="outline"
+                  size="icon"
+                  onClick={() => handleRemoveBookmark(bookmarkId)}
+                >
+                  <FcBookmark className="inline-block" size={36} />
+                </Button>
+              ) : (
+                <Button
+                  variant="outline"
+                  size="icon"
+                  onClick={() => handleAddBookmark(article.id)}
+                >
+                  <MdOutlineBookmarkAdd className="inline-block" size={36} />
+                </Button>
+              )}
+            </div>
+          )}
+        </div>
 
+        <div className="my-10 flex justify-around">
+          <Link href={article.articleUrl} target="_blank">
+            <Button
+              size={"lg"}
+              className="w-full bg-blue-700 text-xl text-white hover:bg-blue-900"
+            >
+              READ MORE
+            </Button>
+          </Link>
+          <SheetClose asChild>
+            <Button variant="outline" className="inline-block w-1/3 text-xl">
+              CLOSE
+            </Button>
+          </SheetClose>
+        </div>
+      </SheetHeader>
+
+      <div className="h-[400px] overflow-y-scroll md:h-[400px]">
+        <Link href={article.articleUrl} target="_blank">
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            className="m-auto cursor-pointer rounded-md object-cover object-center pb-8 hover:opacity-80 md:h-[370px]"
+            src={imageUrl}
+            alt=""
+          />
+        </Link>
+
+        <div className="text-lg tracking-wide">
+          <div>{convertParseHtml(article.description)}</div>
+        </div>
+
+        <div className="flex flex-wrap justify-start pb-0">
           {article.feeds.length > 0 &&
             article.feeds.map((feed) => (
               <Link
                 key={`${feed.id}-${feed.category.id}`}
-                className="ml-2 hover:opacity-80"
+                className="mb-2 ml-2 hover:opacity-80"
                 href={feed.siteUrl}
                 target="_blank"
               >
-                <FeedCategoryNameBadge name={feed.category.name} />
+                <FeedNameBadge name={feed.name} />
               </Link>
             ))}
-
-          <span className="pl-2 text-sm">
-            {formatShowDateTime(article.publishedAt)}
-          </span>
         </div>
 
-        {user && (
-          <div>
-            {bookmarkId ? (
-              <Button
-                variant="outline"
-                size="icon"
-                onClick={() => handleRemoveBookmark(bookmarkId)}
-              >
-                <FcBookmark className="inline-block" size={36} />
-              </Button>
-            ) : (
-              <Button
-                variant="outline"
-                size="icon"
-                onClick={() => handleAddBookmark(article.id)}
-              >
-                <MdOutlineBookmarkAdd className="inline-block" size={36} />
-              </Button>
-            )}
-          </div>
-        )}
-
-        <div className="h-[600px] overflow-y-scroll md:h-[500px]">
+        <div className="my-10 flex justify-around">
           <Link href={article.articleUrl} target="_blank">
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img
-              className="m-auto h-[370px] cursor-pointer rounded-md object-cover object-center pb-8 hover:opacity-80"
-              src={imageUrl}
-              alt=""
-            />
+            <Button
+              size={"lg"}
+              className="w-full bg-blue-700 text-xl text-white hover:bg-blue-900"
+            >
+              READ MORE
+            </Button>
           </Link>
-
-          <div className="my-10 text-center">
-            <Link href={article.articleUrl} target="_blank">
-              <Button
-                size={"lg"}
-                className="w-1/2 bg-blue-700 text-xl hover:bg-blue-900"
-              >
-                本文を読む
-              </Button>
-            </Link>
-          </div>
-
-          <div className="text-lg tracking-wide">
-            <div>{convertParseHtml(article.description)}</div>
-          </div>
-
-          <div className="my-10 text-center">
-            <Link href={article.articleUrl} target="_blank">
-              <Button
-                size={"lg"}
-                className="w-1/2 bg-blue-700 text-xl hover:bg-blue-900"
-              >
-                本文を読む
-              </Button>
-            </Link>
-          </div>
+          <SheetClose asChild>
+            <Button variant="outline" className="inline-block w-1/3 text-xl">
+              CLOSE
+            </Button>
+          </SheetClose>
         </div>
-      </SheetHeader>
-      <SheetFooter className="mt-8">
+      </div>
+
+      {/* <SheetFooter className="mt-8">
         <SheetClose asChild>
-          <Button
-            variant="outline"
-            className=" inline-block w-1/2 text-xl hover:bg-blue-900"
-          >
-            閉じる
+          <Button variant="outline" className=" inline-block w-1/2 text-xl ">
+            CLOSE
           </Button>
         </SheetClose>
-      </SheetFooter>
+      </SheetFooter> */}
     </>
   );
 };
