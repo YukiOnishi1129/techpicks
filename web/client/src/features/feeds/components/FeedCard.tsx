a65c832616b9e4dca6c51c38d76c55219707a96d
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2024-05-02 22:43:22 +0900

    feed card

--- web/client/src/features/feeds/components/FeedCard.tsx
+++ web/client/src/features/feeds/components/FeedCard.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import { FC } from "react";
 
 import { useCheckImageExist } from "@/hooks/useImage";
@@ -19,29 +20,35 @@ export const FeedCard: FC<FeedCardProps> = ({ feed }: FeedCardProps) => {
   return (
     <div className="relative h-[450px] w-full cursor-pointer rounded px-4 py-2 md:h-[210px]">
       <div className="mt-2 flex h-10 md:mt-0">
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img
-          className="inline-block size-8 rounded-full shadow-md"
-          src={faviconUrl}
-          alt=""
-        />
+        <Link href={`/feed/${feed.id}`} target="_blank">
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            className="inline-block size-8 rounded-full shadow-md"
+            src={faviconUrl}
+            alt=""
+          />
+        </Link>
       </div>
 
       <div>
         <div className="mt-0 md:mt-4">
-          <h3 className="line-clamp-2 h-[48px] text-left text-lg font-bold tracking-wide md:h-full md:text-xl">
-            {feed.name}
-          </h3>
+          <Link href={`/feed/${feed.id}`} target="_blank">
+            <h3 className="line-clamp-2 h-[48px] text-left text-lg font-bold tracking-wide md:h-full md:text-xl">
+              {feed.name}
+            </h3>
+          </Link>
 
           <div className="mt-2 flex justify-center md:hidden md:w-[30%]">
-            <div className="max-h-[200px] w-full md:h-36 md:max-h-[70px] md:w-48">
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img
-                className="mx-auto h-full rounded-lg border-2 object-cover object-center shadow-md"
-                src={imageUrl}
-                alt=""
-              />
-            </div>
+            <Link href={`/feed/${feed.id}`} target="_blank">
+              <div className="max-h-[200px] w-full md:h-36 md:max-h-[70px] md:w-48">
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img
+                  className="mx-auto h-full rounded-lg border-2 object-cover object-center shadow-md"
+                  src={imageUrl}
+                  alt=""
+                />
+              </div>
+            </Link>
           </div>
 
           {/* <div className="flex w-full items-center pt-2 md:w-4/5">
@@ -55,7 +62,11 @@ export const FeedCard: FC<FeedCardProps> = ({ feed }: FeedCardProps) => {
 
           {/* TODO: description */}
           <div className="mt-2">
-            <p className="line-clamp-3 h-[62px] text-sm">{feed.description}</p>
+            <Link href={`/feed/${feed.id}`} target="_blank">
+              <p className="line-clamp-3 h-[62px] text-sm">
+                {feed.description}
+              </p>
+            </Link>
           </div>
         </div>
 
