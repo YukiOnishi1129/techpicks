bc6bb253abf833c49911fb46ecd40826139e6f64
yukionishi1129 <yuki.onishi1129@gmail.com>
2024-05-07 12:11:04 +0900

    fix company

--- batch-service/cmd/article-company-crawler/main.go
+++ batch-service/cmd/article-company-crawler/main.go
@@ -3,21 +3,25 @@ package main
 import (
 	"context"
 	"database/sql"
+	"log"
+	"os"
+
 	"github.com/YukiOnishi1129/techpicks/batch-service/cmd/article-company-crawler/usecase"
 	"github.com/YukiOnishi1129/techpicks/batch-service/database"
 	"github.com/YukiOnishi1129/techpicks/batch-service/infrastructure/rss/client"
 	"github.com/YukiOnishi1129/techpicks/batch-service/infrastructure/rss/repository"
 	"github.com/joho/godotenv"
-	"log"
 )
 
 func main() {
 	log.Printf("Start batch service")
 	ctx := context.Background()
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-		return
+	if os.Getenv("GO_ENV") != "production" && os.Getenv("GO_ENV") != "staging"{
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatalf("Error loading .env file")
+			return
+		}
 	}
 
 	db, err := database.Init()
