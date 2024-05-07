147cfaddb9461015b3dc6ac757691791604e7c7b
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2024-05-07 16:21:16 +0900

    delete sample directory

--- sample/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { fetchZenn, fetchZennV2 } from "./apis/zenn";
-// import { fetchHatena } from "./apis/hatena";
-import { fetchQiita } from "./apis/qiita";
-import { fetchDevTo } from "./apis/devto";
-
-// fetchZenn();
-// fetchHatena();
-// fetchZennV2();
-// fetchDevTo();
