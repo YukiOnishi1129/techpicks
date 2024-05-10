c82b6761aec02483fe1de4c659b0e3b32a11a68e
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2024-05-10 22:04:06 +0900

    fix link style

--- web/admin/src/components/layouts/Sidebar/index.tsx
+++ web/admin/src/components/layouts/Sidebar/index.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
-import { MdCalendarToday } from "react-icons/md";
+import { DiGoogleCloudPlatform } from "react-icons/di";
+import { FaRegUserCircle } from "react-icons/fa";
+import { MdDashboard, MdRssFeed, MdCategory } from "react-icons/md";
 
 import { AdminUserType } from "@/types/admin";
 
@@ -12,30 +14,45 @@ export const Sidebar = ({ adminUser }: SidebarProps) => {
     <div className="h-lvh w-full overflow-y-auto border-r-2 pb-12">
       <div className="mb-16 space-y-4 py-4">
         <div className="px-4 py-2">
-          <h2 className="mb-2 px-2 text-lg font-semibold tracking-tight">
-            Main
-          </h2>
-          <div className="space-y-2">
-            <div className="flex cursor-pointer items-center space-x-2 rounded-md px-2 hover:bg-secondary">
-              <MdCalendarToday />
-              <Link href="/dashboard">Dashboard</Link>
-            </div>
-            <div className="flex cursor-pointer items-center space-x-2 rounded-md px-2 hover:bg-secondary">
-              <MdCalendarToday />
-              <Link href="/platform">Platform</Link>
-            </div>
-            <div className="flex cursor-pointer items-center space-x-2 rounded-md px-2 hover:bg-secondary">
-              <MdCalendarToday />
-              <Link href="/feed">Feed</Link>
-            </div>
-            <div className="flex cursor-pointer items-center space-x-2 rounded-md px-2 hover:bg-secondary">
-              <MdCalendarToday />
-              <Link href="/category">Category</Link>
-            </div>
-            <div className="flex cursor-pointer items-center space-x-2 rounded-md px-2 hover:bg-secondary">
-              <MdCalendarToday />
-              <Link href="/user">User</Link>
-            </div>
+          <div className="space-y-8 text-xl">
+            <Link
+              href="/dashboard"
+              className="flex cursor-pointer items-center space-x-2 rounded-md p-2 hover:bg-secondary"
+            >
+              <MdDashboard />
+              <span>Dashboard</span>
+            </Link>
+
+            <Link
+              href="/platform"
+              className="flex cursor-pointer items-center space-x-2 rounded-md p-2 hover:bg-secondary"
+            >
+              <DiGoogleCloudPlatform />
+              <span>Platform</span>
+            </Link>
+            <Link
+              href="/feed"
+              className="flex cursor-pointer items-center space-x-2 rounded-md p-2 hover:bg-secondary"
+            >
+              <MdRssFeed />
+              <span>Feed</span>
+            </Link>
+
+            <Link
+              href="/category"
+              className="flex cursor-pointer items-center space-x-2 rounded-md p-2 hover:bg-secondary"
+            >
+              <MdCategory />
+              <span>Category</span>
+            </Link>
+
+            <Link
+              href="/user"
+              className="flex cursor-pointer items-center space-x-2 rounded-md p-2 hover:bg-secondary"
+            >
+              <FaRegUserCircle />
+              <span>User</span>
+            </Link>
           </div>
         </div>
 
