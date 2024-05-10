1adbf576ad354220929047b4bc49b4354d14df7a
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2024-05-10 22:50:18 +0900

    table demo

--- web/admin/src/components/layouts/BaseLayout/MainBaseLayout.tsx
+++ web/admin/src/components/layouts/BaseLayout/MainBaseLayout.tsx
@@ -26,7 +26,7 @@ export const MainBaseLayout: FC<MainBaseLayoutProps> = async ({
       className="min-h-screen w-full rounded-lg border"
     >
       <ResizablePanel defaultSize={10} className="max-h-[60px] min-h-[60px]">
-        {adminUser && <Header adminUser={adminUser} />}
+        <Header adminUser={adminUser} />
       </ResizablePanel>
       <ResizableHandle />
       <ResizablePanel defaultSize={90}>
@@ -38,16 +38,11 @@ export const MainBaseLayout: FC<MainBaseLayoutProps> = async ({
             defaultSize={25}
             className="min-w-[200px] max-w-[200px]"
           >
-            {/* <div className="flex h-full items-center justify-center p-6">
-              <span className="font-semibold">Sidebar</span>
-            </div> */}
-            {adminUser && <Sidebar adminUser={adminUser} />}
+            <Sidebar adminUser={adminUser} />
           </ResizablePanel>
           <ResizableHandle withHandle />
           <ResizablePanel defaultSize={75}>
-            <div className="flex h-full items-center justify-center p-6">
-              <span className="font-semibold">{children}</span>
-            </div>
+            <div className="h-full p-6">{children}</div>
           </ResizablePanel>
         </ResizablePanelGroup>
       </ResizablePanel>
--- web/admin/src/features/platform/components/PlatformTemplate.tsx
+++ web/admin/src/features/platform/components/PlatformTemplate.tsx
@@ -1,16 +1,18 @@
 import { FC } from "react";
 
+import { PlatformTable } from "./PlatformDataTable";
 import { fetchPlatformsAPI } from "../actions/platform";
 
 type PlatformTemplateProps = {};
 
 export const PlatformTemplate: FC<PlatformTemplateProps> = async () => {
   const res = await fetchPlatformsAPI();
-  console.log("🔥");
-  console.log(res.data.platforms);
   return (
     <div>
       <h1>Platform Template</h1>
+      <div>
+        <PlatformTable />
+      </div>
     </div>
   );
 };
