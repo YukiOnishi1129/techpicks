e69a84bbc99bb295ad83f405ef19ccad55ce0889
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2024-05-10 17:48:30 +0900

    side bar

--- web/admin/src/components/layouts/BaseLayout/index.tsx
+++ web/admin/src/components/layouts/BaseLayout/index.tsx
@@ -23,9 +23,25 @@ export const BaseLayout: FC<BaseLayoutProps> = ({ children }) => {
       </ResizablePanel>
       <ResizableHandle />
       <ResizablePanel defaultSize={90}>
-        <div className="flex h-full items-center justify-center p-6">
-          <span className="font-semibold">{children}</span>
-        </div>
+        <ResizablePanelGroup
+          direction="horizontal"
+          className="w-full rounded-lg border"
+        >
+          <ResizablePanel
+            defaultSize={25}
+            className="min-w-[200px] max-w-[200px]"
+          >
+            <div className="flex h-full items-center justify-center p-6">
+              <span className="font-semibold">Sidebar</span>
+            </div>
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel defaultSize={75}>
+            <div className="flex h-full items-center justify-center p-6">
+              <span className="font-semibold">Content</span>
+            </div>
+          </ResizablePanel>
+        </ResizablePanelGroup>
       </ResizablePanel>
     </ResizablePanelGroup>
   );
