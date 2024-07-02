2f1379990359e2f8c38f6f3b4a124af9a130b359
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2024-07-02 17:18:21 +0900

    graphql

--- bff/apollo-gateway/.eslintrc.js
+++ bff/apollo-gateway/.eslintrc.js
@@ -5,10 +5,18 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: [
+    '@typescript-eslint/eslint-plugin',
+    'import',
+    'sort-keys-fix',
+    'typescript-sort-keys',
+    'unused-imports',
+  ],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'prettier',
+    'eslint:recommended',
   ],
   root: true,
   env: {
@@ -21,5 +29,26 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          ['parent', 'sibling', 'index'],
+          'object',
+          'type',
+        ],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+        },
+      },
+    ],
+    'import/no-duplicates': 'error',
+    'sort-keys-fix/sort-keys-fix': 'error',
+    'typescript-sort-keys/interface': 'error',
+    'unused-imports/no-unused-imports': 'error',
   },
 };
