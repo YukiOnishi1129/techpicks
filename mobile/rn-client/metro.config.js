0ca3addbe0ede243f3e6396347067e4fb31889c0
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2024-07-13 18:26:49 +0900

    fix lint

--- mobile/rn-client/metro.config.js
+++ mobile/rn-client/metro.config.js
@@ -3,6 +3,6 @@ const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
 /** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname); // eslint-disable-line no-undef
 
 module.exports = withNativeWind(config, { input: "./global.css" });
