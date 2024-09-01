3c65b067a6383d7337e9011318a392a6804be15e
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2024-09-01 14:05:05 +0900

    change directory cmd

--- micro-service/content-service/domain/feed.go
+++ micro-service/content-service/domain/feed.go
@@ -1,15 +1,1 @@
 package domain
-
-import "time"
-
-type Feed struct {
-	ID                string
-	Name              string
-	RssURL            string
-	TrendPlatformType TrendPlatformType
-	Platform          Platform
-	Category          Category
-	CreatedAt         time.Time
-	UpdatedAt         time.Time
-	DeletedAt         *time.Time
-}
--- micro-service/content-service/domain/platform.go
+++ micro-service/content-service/domain/platform.go
@@ -1,9 +1,5 @@
 package domain
 
-import (
-	"time"
-)
-
 type PlatformSiteType int
 
 const (
@@ -12,15 +8,3 @@ const (
 	PlatformTypeCompany
 	PlatformTypeSummary
 )
-
-type Platform struct {
-	ID               string
-	Name             string
-	SiteURL          string
-	PlatformSiteType PlatformSiteType
-	IsEng            bool
-	FaviconURL       string
-	CreatedAt        time.Time
-	UpdatedAt        time.Time
-	DeletedAt        *time.Time
-}
