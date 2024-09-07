20438b912d69ab044674944da3d7347e0b6f4818
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2024-09-07 20:12:25 +0900

    fix

--- bff/apollo-gateway/src/app/article/article.module.ts
+++ bff/apollo-gateway/src/app/article/article.module.ts
@@ -4,24 +4,7 @@ import { ArticleResolver } from './article.resolver';
 import { ArticleService } from './article.service';
 
 @Module({
-  imports: [
-    // ClientsModule.register([
-    //   {
-    //     name: 'ARTICLE_PACKAGE',
-    //     options: {
-    //       credentials: grpc.credentials.createInsecure(),
-    //       package: 'checkpicks.content.v1',
-    //       protoPath: join(__dirname, '../../proto/content/content.proto'),
-    //       url:
-    //         process.env.NODE_ENV !== 'production' &&
-    //         process.env.NODE_ENV !== 'staging'
-    //           ? `${process.env.CONTENT_SERVICE_CONTAINER_NAME}:${process.env.CONTENT_SERVICE_CONTAINER_PORT}`
-    //           : process.env.CONTENT_SERVICE_CONTAINER_NAME,
-    //     },
-    //     transport: Transport.GRPC,
-    //   },
-    // ]),
-  ],
+  imports: [],
   providers: [ArticleResolver, ArticleService],
 })
 export class ArticleModule {}
