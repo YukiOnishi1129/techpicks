9ccece884f665925e55881d4b578dac9c3524511
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2024-09-12 21:01:50 +0900

    deletebookmark

---
+++ web/client-v2/src/features/bookmarks/hooks/useBookmark.ts
@@ -0,0 +1,56 @@
+import { useCallback } from "react";
+
+import { logoutToLoginPage } from "@/features/auth/actions/auth";
+import { getUser } from "@/features/auth/actions/user";
+
+import { useStatusToast } from "@/hooks/useStatusToast";
+
+import { serverRevalidatePage } from "@/actions/serverRevalidatePage";
+
+import { deleteBookmarkMutation } from "../actions/deleteBookmarkMutation";
+
+export const useBookmark = (pathname: string) => {
+  const { successToast, failToast } = useStatusToast();
+
+  const handleRemoveBookmark = useCallback(
+    async (bookmarkId: string) => {
+      const user = await getUser();
+      if (!user) {
+        failToast({
+          description: "Fail: Please login to remove bookmark",
+        });
+        await logoutToLoginPage();
+        return;
+      }
+
+      if (!bookmarkId) return;
+
+      const { data, error } = await deleteBookmarkMutation({
+        bookmarkId: bookmarkId,
+        userId: user?.id || "",
+      });
+      let errMsg = "";
+      if (error) {
+        errMsg = "Fail: Something went wrong";
+        if (error.length > 0) {
+          errMsg = error[0].message;
+        }
+      }
+      if (!data?.deleteBookmark) errMsg = "Fail: Something went wrong";
+      if (errMsg !== "") {
+        failToast({
+          description: errMsg,
+        });
+        return;
+      }
+
+      successToast({
+        description: "Remove bookmark",
+      });
+      await serverRevalidatePage(pathname);
+    },
+    [successToast, failToast, pathname]
+  );
+
+  return { handleRemoveBookmark };
+};
