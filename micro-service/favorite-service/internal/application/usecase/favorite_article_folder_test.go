0b645419ffdb63ab92b3ac02ee78eb2857ffa84b
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2024-09-15 01:43:39 +0900

    keyword

--- micro-service/favorite-service/internal/application/usecase/favorite_article_folder_test.go
+++ micro-service/favorite-service/internal/application/usecase/favorite_article_folder_test.go
@@ -212,6 +212,148 @@ func Test_UseCase_GetFavoriteArticleFolders(t *testing.T) {
 				},
 			},
 		},
+		"Success: keyword": {
+			recordFavoriteArticleFolders: []entity.FavoriteArticleFolder{
+				{
+					ID:     fafID1.String(),
+					UserID: userID1,
+					Title:  "faf_title1",
+					Description: null.String{
+						Valid:  true,
+						String: "faf_description1",
+					},
+				},
+				{
+					ID:     fafID2.String(),
+					UserID: userID1,
+					Title:  "faf_title2",
+					Description: null.String{
+						Valid:  true,
+						String: "faf_description2",
+					},
+				},
+				{
+					ID:     fafID3.String(),
+					UserID: userID2,
+					Title:  "faf_title3",
+					Description: null.String{
+						Valid:  true,
+						String: "faf_description3",
+					},
+				},
+				{
+					ID:     fafID4.String(),
+					UserID: userID1,
+					Title:  "faf_title4",
+					Description: null.String{
+						Valid:  true,
+						String: "faf_description4",
+					},
+				},
+			},
+			recordFavoriteArticles: []entity.FavoriteArticle{
+				{
+					ID:                      faID1.String(),
+					UserID:                  userID1,
+					FavoriteArticleFolderID: fafID1.String(),
+					PlatformID: null.String{
+						Valid:  true,
+						String: platformID1,
+					},
+					ArticleID:   articleID1,
+					Title:       "fa_title1",
+					Description: "fa_description1",
+					ArticleURL:  "https://example.com/article1",
+					PublishedAt: null.TimeFrom(time.Unix(publishedAt, 0)),
+					AuthorName: null.String{
+						Valid:  true,
+						String: "author1",
+					},
+					Tags: null.String{
+						Valid:  true,
+						String: "tag1",
+					},
+					ThumbnailURL:       "https://example.com/thumbnail1",
+					PlatformName:       "platform1",
+					PlatformURL:        "https://example.com/platform1",
+					PlatformFaviconURL: "https://example.com/favicon1",
+					IsEng:              true,
+					IsRead:             false,
+				},
+				{
+					ID:                      faID2.String(),
+					UserID:                  userID1,
+					FavoriteArticleFolderID: fafID1.String(),
+					ArticleID:               articleID2,
+					Title:                   "fa_title2",
+					Description:             "fa_description2",
+					ArticleURL:              "https://example.com/article2",
+					ThumbnailURL:            "https://example.com/thumbnail2",
+					PlatformName:            "platform2",
+					PlatformURL:             "https://example.com/platform2",
+					PlatformFaviconURL:      "https://example.com/favicon2",
+					IsEng:                   true,
+					IsRead:                  false,
+				},
+			},
+			arg: &fpb.GetFavoriteArticleFoldersRequest{
+				UserId:  userID1,
+				Keyword: &wrapperspb.StringValue{Value: "e1"},
+			},
+			want: &fpb.GetFavoriteArticleFoldersResponse{
+				FavoriteArticleFoldersEdge: []*fpb.FavoriteArticleFolderEdge{
+					{
+						Node: &fpb.FavoriteArticleFolder{
+							Id:          fafID1.String(),
+							UserId:      userID1,
+							Title:       "faf_title1",
+							Description: "faf_description1",
+							FavoriteArticles: []*fpb.FavoriteArticle{
+								{
+									Id:                      faID1.String(),
+									UserId:                  userID1,
+									FavoriteArticleFolderId: fafID1.String(),
+									PlatformId:              &wrapperspb.StringValue{Value: platformID1},
+									ArticleId:               articleID1,
+									Title:                   "fa_title1",
+									Description:             "fa_description1",
+									ArticleUrl:              "https://example.com/article1",
+									PublishedAt:             &timestamppb.Timestamp{Seconds: publishedAt},
+									AuthorName:              &wrapperspb.StringValue{Value: "author1"},
+									Tags:                    &wrapperspb.StringValue{Value: "tag1"},
+									ThumbnailUrl:            "https://example.com/thumbnail1",
+									PlatformName:            "platform1",
+									PlatformUrl:             "https://example.com/platform1",
+									PlatformFaviconUrl:      "https://example.com/favicon1",
+									IsEng:                   true,
+									IsRead:                  false,
+								},
+								{
+									Id:                      faID2.String(),
+									UserId:                  userID1,
+									FavoriteArticleFolderId: fafID1.String(),
+									ArticleId:               articleID2,
+									Title:                   "fa_title2",
+									Description:             "fa_description2",
+									ArticleUrl:              "https://example.com/article2",
+									ThumbnailUrl:            "https://example.com/thumbnail2",
+									PlatformName:            "platform2",
+									PlatformUrl:             "https://example.com/platform2",
+									PlatformFaviconUrl:      "https://example.com/favicon2",
+									IsEng:                   true,
+									IsRead:                  false,
+								},
+							},
+						},
+						Cursor: fafID1.String(),
+					},
+				},
+				PageInfo: &fpb.PageInfo{
+					HasNextPage: false,
+					EndCursor:   fafID4.String(),
+				},
+			},
+		},
 	}
 
 	for name, tt := range test {
