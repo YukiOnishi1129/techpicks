4757113896b8924d2afb58c376bef639a5a5d555
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2024-09-15 20:48:38 +0900

    add getFavoriteArticleFolderListQuery

---
+++ web/client-v2/src/features/favorites/actions/getFavoriteArticleFolderListQuery.ts
@@ -0,0 +1,57 @@
+"use server";
+
+import { graphql, readFragment } from "gql.tada";
+
+import { getClient } from "@/lib/apollo/client";
+
+import { FavoriteArticleFoldersInput } from "@/graphql/type";
+
+const FavoriteArticleFolderListTemplateFragment = graphql(`
+  fragment FavoriteArticleFolderListTemplateFragment on Query {
+    favoriteArticleFolders(input: $input) {
+      pageInfo {
+        hasNextPage
+        hasPreviousPage
+        startCursor
+        endCursor
+      }
+      edges {
+        node {
+          id
+          title
+          description
+        }
+      }
+    }
+  }
+`);
+
+const FavoriteArticleFolderListQuery = graphql(
+  `
+    query FavoriteArticleFolderListQuery($input: FavoriteArticleFoldersInput!) {
+      ...FavoriteArticleFolderListTemplateFragment
+    }
+  `,
+  [FavoriteArticleFolderListTemplateFragment]
+);
+
+export const getFavoriteArticleFolderListQuery = async (
+  input: FavoriteArticleFoldersInput
+) => {
+  const { data, error, loading } = await getClient().query({
+    query: FavoriteArticleFolderListQuery,
+    context: {
+      fetchOptions: {
+        cache: "no-cache",
+      },
+    },
+    variables: {
+      input,
+    },
+    errorPolicy: "all",
+  });
+
+  const newData = readFragment(FavoriteArticleFolderListTemplateFragment, data);
+
+  return { data: newData, error, loading };
+};
--- web/client-v2/src/features/favorites/components/Template/FavoriteArticleFolderListTemplate/FavoriteArticleFolderListTemplate.tsx
+++ web/client-v2/src/features/favorites/components/Template/FavoriteArticleFolderListTemplate/FavoriteArticleFolderListTemplate.tsx
@@ -1,5 +1,10 @@
 import { User } from "@supabase/supabase-js";
 import { FC } from "react";
+import { FaHeart } from "react-icons/fa";
+
+import { getFavoriteArticleFolderListQuery } from "@/features/favorites/actions/getFavoriteArticleFolderListQuery";
+
+import { CreateFavoriteArticleFolderDialog } from "../../Dialog";
 
 type FavoriteArticleFolderListTemplateProps = {
   user: User;
@@ -8,6 +13,36 @@ type FavoriteArticleFolderListTemplateProps = {
 
 export const FavoriteArticleFolderListTemplate: FC<
   FavoriteArticleFolderListTemplateProps
-> = ({ user, keyword }) => {
-  return <div>Favorite</div>;
+> = async ({ user, keyword }) => {
+  const { data } = await getFavoriteArticleFolderListQuery({ keyword });
+  console.log("🔥");
+  console.log(data.favoriteArticleFolders.edges);
+  return (
+    <div>
+      <div className="fixed z-10 hidden w-[90%] bg-card md:block md:w-[70%] md:px-4">
+        <h1 className="mt-4 flex items-center text-2xl font-bold">
+          <FaHeart className="mr-4" color="red" />
+          <span>Favorite Folders</span>
+        </h1>
+        <div className="mt-2 flex w-full items-center justify-between">
+          <div className="w-4/5 pr-4">
+            {/* <FavoriteArticleFolderKeywordSearchInput keyword={keyword} /> */}
+          </div>
+          <div>
+            <CreateFavoriteArticleFolderDialog />
+          </div>
+        </div>
+      </div>
+      <div className="h-4 md:h-[120px]" />
+
+      {/* <FavoriteArticleFolderList
+        initialFavoriteArticleFolders={res.data.favoriteArticleFolders}
+        user={user}
+      /> */}
+
+      {/* <div className="fixed bottom-20 right-4 z-50 md:hidden">
+        <FavoriteArticleFolderKeyWordSearchDialog keyword={keyword} />
+      </div> */}
+    </div>
+  );
 };
