454e02243f10e1f111a03b781de38b69ffc4b5df
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2024-09-22 04:25:16 +0900

    fix

--- web/client-v2/src/features/favorites/actions/actCreateFavoriteArticleForUploadArticleMutation.ts
+++ web/client-v2/src/features/favorites/actions/actCreateFavoriteArticleForUploadArticleMutation.ts
@@ -2,6 +2,10 @@
 
 import { graphql } from "gql.tada";
 
+import { getClient } from "@/lib/apollo/client";
+
+import { CreateFavoriteArticleForUploadArticleInput } from "@/graphql/type";
+
 const CreateFavoriteArticleForUploadArticleMutation = graphql(`
   mutation CreateFavoriteArticleForUploadArticleMutation(
     $input: CreateFavoriteArticleForUploadArticleInput!
@@ -11,3 +15,23 @@ const CreateFavoriteArticleForUploadArticleMutation = graphql(`
     }
   }
 `);
+
+export const createFavoriteArticleForUploadArticleMutation = async (
+  input: CreateFavoriteArticleForUploadArticleInput
+) => {
+  const createFavoriteArticleForUploadArticle = getClient().mutate({
+    mutation: CreateFavoriteArticleForUploadArticleMutation,
+    context: {
+      fetchOptions: {
+        cache: "no-store",
+      },
+    },
+    variables: {
+      input,
+    },
+    errorPolicy: "all",
+  });
+
+  const res = await createFavoriteArticleForUploadArticle;
+  return { data: res.data, error: res.errors };
+};
