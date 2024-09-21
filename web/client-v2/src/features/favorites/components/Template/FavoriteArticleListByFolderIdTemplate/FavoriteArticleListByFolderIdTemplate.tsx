b43684abe7c31c62b2caa5a3790a759899a073c0
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2024-09-22 03:21:32 +0900

    fix

--- web/client-v2/src/features/favorites/components/Template/FavoriteArticleListByFolderIdTemplate/FavoriteArticleListByFolderIdTemplate.tsx
+++ web/client-v2/src/features/favorites/components/Template/FavoriteArticleListByFolderIdTemplate/FavoriteArticleListByFolderIdTemplate.tsx
@@ -14,6 +14,7 @@ import {
   FavoriteArticleFoldersByFolderIdTemplateFragment,
   FavoriteArticleListByFolderIdTemplateFragment,
 } from "./FavoriteArticleListByFolderIdTemplateFragment";
+import { CreateFavoriteArticleDialog } from "../../Dialog";
 import { FavoriteArticleList } from "../../List/FavoriteArticleList/FavoriteArticleList";
 import { FavoriteArticleKeywordSearchForm } from "../../Search";
 
@@ -93,10 +94,10 @@ export const FavoriteArticleListByFolderIdTemplate: FC<
               keyword={keyword}
             />
           </div>
-          {/* <CreateFavoriteArticleDialog
+          <CreateFavoriteArticleDialog
             user={user}
             favoriteArticleFolderId={id}
-          /> */}
+          />
         </div>
       </div>
 
