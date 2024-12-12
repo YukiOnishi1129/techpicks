23893c5075e16149c3b06782c1ff3f44ac85da50
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2024-12-12 17:44:26 +0900

    feat(web): add feed page

--- web/client-v2/src/app/feed/page.tsx
+++ web/client-v2/src/app/feed/page.tsx
@@ -1,7 +1,12 @@
+import { redirect } from "next/navigation";
+
 import { getUser } from "@/features/auth/actions/user";
 
 export default async function FeedPage() {
   const user = await getUser();
+  if (!user) {
+    redirect("/login");
+  }
   return (
     <div>
       <h1>Feed</h1>
