b8b0c0c6ed7927cb0f0a58504663b0fd16489f6e
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2025-01-01 05:39:49 +0900

    feat(content): GetAllFeeds rpc

--- micro-service/content-service/internal/interfacess/handler/feed.go
+++ micro-service/content-service/internal/interfacess/handler/feed.go
@@ -14,6 +14,14 @@ func (ch *contentHandler) GetFeeds(ctx context.Context, req *cpb.GetFeedsRequest
 	return res, nil
 }
 
+func (ch *contentHandler) GetAllFeeds(ctx context.Context, req *cpb.GetAllFeedsRequest) (*cpb.GetFeedsResponse, error) {
+	// res, err := ch.contentUseCase.CreateUploadArticle(ctx, req)
+	// if err != nil {
+	// 	return nil, err
+	// }
+	return nil, nil
+}
+
 func (ch *contentHandler) GetFeed(ctx context.Context, req *cpb.GetFeedRequest) (*cpb.GetFeedResponse, error) {
 	res, err := ch.contentUseCase.GetFeed(ctx, req)
 	if err != nil {
