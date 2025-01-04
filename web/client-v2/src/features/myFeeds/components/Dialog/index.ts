f765cf2277c507ba0d6f6343a355d6ecb86131d1
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2025-01-04 17:45:40 +0900

    SearchMyFeedFolderDialog

--- web/client-v2/src/features/myFeeds/components/Dialog/index.ts
+++ web/client-v2/src/features/myFeeds/components/Dialog/index.ts
@@ -2,4 +2,5 @@ export * from "./CreateMyFeedFolderDialog";
 export * from "./UpdateMyFeedFolderDialog";
 export * from "./ShowMyFeedListDialog";
 
+export * from "./SearchMyFeedFolderDialog";
 export * from "./SearchMyFeedFolderArticleDialog";
