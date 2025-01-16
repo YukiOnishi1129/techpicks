6f3cea951c638538febac26956fa528ebebc8017
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2025-01-16 17:17:31 +0900

    feat(content):add rpc comment

--- micro-service/content-service/internal/interfacess/handler/comment.go
+++ micro-service/content-service/internal/interfacess/handler/comment.go
@@ -4,9 +4,18 @@ import (
 	"context"
 
 	cpb "github.com/YukiOnishi1129/checkpicks-protocol-buffers/checkpicks-rpc-go/grpc/content"
+	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 func (ch *contentHandler) UpsertArticleComment(ctx context.Context, req *cpb.UpsertArticleCommentRequest) (*cpb.UpsertArticleCommentResponse, error) {
+	// res, err := ch.contentUseCase.ListComment(ctx, req)
+	// if err != nil {
+	// 	return nil, err
+	// }
+	return nil, nil
+}
+
+func (ch *contentHandler) DeleteArticleComment(ctx context.Context, req *cpb.DeleteArticleCommentRequest) (*emptypb.Empty, error) {
 	// res, err := ch.contentUseCase.ListComment(ctx, req)
 	// if err != nil {
 	// 	return nil, err
